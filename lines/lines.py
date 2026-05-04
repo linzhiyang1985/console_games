779aa08bd984dc6feb5a7bf2cb457302051b052a
linzhiyang1985 <linzhiyang1985@gmail.com>
2026-05-04 21:36:25 +0800

    Add files via upload

---
+++ lines/lines.py
@@ -0,0 +1,367 @@
+from stages.color import Color
+from importlib import import_module
+import msvcrt
+import ctypes
+import time
+import os
+
+class Dot:
+    def __init__(self, is_end_point:bool=False, color:Color=Color.WHITE):
+        self.is_end_point:bool = is_end_point
+        self.color:Color = color
+        self.is_selected:bool = False
+        self.joints:list = [' '] * 6 # up, right * 2, down, left * 2
+        self.is_connected:bool = False
+    
+    def set_joint(self, dir:str):
+        if dir == 'up':
+            self.joints[0] = '│'
+        elif dir == 'right':
+            self.joints[1] = '─'
+            self.joints[2] = '─'
+        elif dir == 'down':
+            self.joints[3] = '│'
+        elif dir == 'left':
+            self.joints[4] = '─'
+            self.joints[5] = '─'
+    
+    def clear_joint(self, dir:str):
+        if dir == 'up':
+            self.joints[0] = ' '
+        elif dir == 'right':
+            self.joints[1] = ' '
+            self.joints[2] = ' '
+        elif dir == 'down':
+            self.joints[3] = ' '
+        elif dir == 'left':
+            self.joints[4] = ' '
+            self.joints[5] = ' '
+    
+    def set_select(self, is_selected:bool):
+        self.is_selected = is_selected
+    
+    def set_connect(self, is_connected:bool):
+        self.is_connected = is_connected
+    
+    def set_color(self, color:Color=Color.WHITE):
+        if not self.is_end_point:
+            self.color = color
+    
+    def get_color(self) -> Color:
+        return self.color
+    
+    def clear_joints(self):
+        self.joints = [' '] * 6
+
+    def render(self):
+        # 3*3
+        content = self.color + ('●' if self.is_end_point else '∙') + Color.RESET
+        rows = [[' ', ' ', self.joints[0], ' ', ' '],
+                [self.joints[5], self.joints[4], content, self.joints[1], self.joints[2]], # ∙ ● ⬤
+                [' ', ' ', self.joints[3],' ', ' ']]
+        if self.is_selected:
+            rows[0][0] = Color.YELLOW + '╭' + Color.RESET
+            rows[0][-1] = Color.YELLOW + '╮' + Color.RESET
+            rows[-1][0] = Color.YELLOW + '╰' + Color.RESET
+            rows[-1][-1] = Color.YELLOW + '╯' + Color.RESET
+        if self.is_connected:
+            for row_index in range(len(rows)):
+                for col_index in range(len(rows[row_index])):
+                    if rows[row_index][col_index] != ' ':
+                        rows[row_index][col_index] = self.color + rows[row_index][col_index] + Color.RESET
+        return rows
+
+
+class Deck:
+    def __init__(self, stage_id):
+        self.array, self.height, self.width, self.end_point_positions = self._load_stage(stage_id) #[[Dot()] * width for _ in range(height)]
+        self.cursor_pos = (0,0)
+        dot = self.get_dot(*self.cursor_pos)
+        dot.set_select(True)
+        self.links = {}
+    
+    def _load_stage(self, stage_id):
+        end_point_positions = []
+        stage = import_module(f'stages.{stage_id}', package=f'stages.{stage_id}')
+        # rows, cols
+        height, width = stage.size
+        array = []
+        for row_index, row in enumerate(stage.deck):
+            row_of_dots = []
+            for col_index, dot_color in enumerate(row):
+                if dot_color is None:
+                    row_of_dots.append(Dot())
+                else:
+                    row_of_dots.append(Dot(is_end_point=True, color=dot_color))
+                    end_point_positions.append((row_index, col_index))
+            array.append(row_of_dots)
+        return array, height, width, end_point_positions
+    
+    def print_deck(self):
+        for row in self.array:
+            dots_in_row = []
+            for dot in row:
+                dots_in_row.append(dot.render())
+            lines_in_row = []
+            for i in range(3): # 每个dot占3行
+                line = []
+                for dot in dots_in_row:
+                    line += dot[i] # 每行包含5个字符
+                lines_in_row.append(line)
+            for line in lines_in_row:
+                print(''.join(line))
+
+    def get_dot(self, row_index, col_index) -> Dot|None:
+        if 0<=row_index<self.width and 0<=col_index<self.height:
+            return self.array[row_index][col_index]
+        else:
+            return None
+    
+    def get_end_points(self) -> list[Dot]:
+        return [self.get_dot(*end_point) for end_point in self.end_point_positions]
+
+    def get_cursor(self) -> tuple[int,int]:
+        return self.cursor_pos
+
+    def get_dot_at_cursor(self) -> Dot|None:
+        return self.get_dot(*self.cursor_pos)
+    
+    def move_cursor(self, row_index, col_index):
+        new_dot = self.get_dot(row_index, col_index)
+        if new_dot is None:
+            # 新坐标越界
+            return False
+        
+        old_dot = self.get_dot_at_cursor()
+        old_dot.set_select(False)
+        new_dot.set_select(True)
+        self.cursor_pos = (row_index, col_index)
+        return True
+    
+    def move_up(self):
+        return self.move_cursor(self.cursor_pos[0] - 1, self.cursor_pos[1])
+    
+    def move_down(self):
+        return self.move_cursor(self.cursor_pos[0] + 1, self.cursor_pos[1])
+    
+    def move_left(self):
+        return self.move_cursor(self.cursor_pos[0], self.cursor_pos[1] - 1)
+    
+    def move_right(self):
+        return self.move_cursor(self.cursor_pos[0], self.cursor_pos[1] + 1)
+
+    def move_cursor_by_direction(self, direction):
+        if direction == 'up':
+            return self.move_up()
+        elif direction == 'right':
+            return self.move_right()
+        elif direction == 'down':
+            return self.move_down()
+        elif direction == 'left':
+            return self.move_left()
+        else:
+            return False
+    
+    def _relative_position(self, r1, c1, r2, c2) -> str|None:
+        if r1 == r2:
+            return 'left' if c1 > c2 else 'right'
+        elif c1 == c2:
+            return 'up' if r1 > r2 else 'down'
+        else:
+            return None
+    
+    def get_link(self, link_color) -> list[tuple[int,int]]:
+        if link_color not in self.links:
+            return []
+        return self.links[link_color]
+    
+    def set_link(self, link_color, dot_positions):
+        self.links[link_color] = dot_positions
+
+        if self.get_dot(*dot_positions[0]).is_end_point and \
+           self.get_dot(*dot_positions[-1]).is_end_point:
+           connected = True
+        else:
+            connected = False
+        
+        for i in range(len(dot_positions)):
+            dot_prev = self.get_dot(*dot_positions[i])
+            if i < len(dot_positions) - 1:
+                dot_next = self.get_dot(*dot_positions[i+1])
+            else:
+                dot_next = None
+            if dot_next:
+                # calculate relative position between these two dots
+                prev_joint_direction = self._relative_position(*dot_positions[i], *dot_positions[i+1])
+                dot_prev.set_joint(prev_joint_direction)
+                if connected:
+                    dot_prev.set_connect(True)
+                    dot_prev.set_color(link_color)
+                else:
+                    dot_prev.set_connect(False)
+                next_joint_direction = self._relative_position(*dot_positions[i+1], *dot_positions[i])
+                dot_next.set_joint(next_joint_direction)
+                if connected:
+                    dot_next.set_connect(True)
+                    dot_next.set_color(link_color)
+                else:
+                    dot_next.set_connect(False)
+    
+    def restart(self):
+        self.links = {}
+        self.cursor_pos = (0, 0)
+    
+    def clear_link_segment(self, dot_pos_a, dot_pos_b, reset_color_a=True, reset_color_b=True):
+        # calculate relative position between these two dots
+        prev_joint_direction = self._relative_position(*dot_pos_a, *dot_pos_b)
+        dot_a = self.get_dot(*dot_pos_a)
+        dot_a.clear_joint(prev_joint_direction)
+        dot_a.set_connect(False)
+        if reset_color_a:
+            dot_a.set_color()
+        next_joint_direction = self._relative_position(*dot_pos_b, *dot_pos_a)
+        dot_b = self.get_dot(*dot_pos_b)
+        dot_b.clear_joint(next_joint_direction)
+        dot_b.set_connect(False)
+        if reset_color_b:
+            dot_b.set_color()
+
+    def clear_link(self, link_color):
+        if link_color not in self.links:
+            self.links[link_color] = []
+            return
+        dot_positions = self.links[link_color]
+        for i in range(len(dot_positions) - 1, 0, -1):
+            self.clear_link_segment(dot_positions[i-1], dot_positions[i])
+        dot_positions.clear()
+
+
+class LineGame:
+    def __init__(self, stage_id):
+        self.stage_id = stage_id
+        self.deck = Deck(stage_id)
+
+    def get_user_input(self):
+        accepted_keys_and_map = {
+            b'H': 'up', b'P': 'down', b'K': 'left', b'M': 'right',
+            b'q': 'quit',
+            b'r': 'restart',
+        }
+        while True:
+            if msvcrt.kbhit():
+                key = msvcrt.getch()
+                if key == b'\xe0':
+                    key = msvcrt.getch()
+                if key in accepted_keys_and_map:
+                    cmd = accepted_keys_and_map[key]
+                    if (ctypes.windll.user32.GetKeyState(0x10) & 0x8000) != 0:
+                        cmd += ' link'
+                    return cmd
+
+            time.sleep(0.1)
+    
+    def clear(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+    
+    def move_selector(self, direction):
+        return self.deck.move_cursor_by_direction(direction)
+    
+    def print_title(self):
+        print(f'====连线游戏(第{self.stage_id}关)====')
+        print()
+    
+    def _move_cursor(self, row: int, column: int):
+        """移动光标到指定位置"""
+        if os.name == 'nt':
+            msvcrt.putch(b'\x1b')
+            msvcrt.putch(b'[')
+            for r in f"{row}":
+                msvcrt.putch(r.encode())
+            msvcrt.putch(b';')
+            for c in f"{column}":
+                msvcrt.putch(c.encode())
+            msvcrt.putch(b'H')
+        else:
+            curses.move(row, column)
+
+    def print_deck(self):
+        self._move_cursor(2, 0)
+        self.deck.print_deck()
+    
+    def check_win(self):
+        end_points = self.deck.get_end_points()
+        for end_point in end_points:
+            if not end_point.is_connected:
+                return False
+        return True
+
+    def play(self):
+        self.clear()
+        self.print_title()
+        self.print_deck()
+        while True:
+            direction = self.get_user_input()
+            cmd = direction.split(' ')
+            if cmd[0] == 'quit':
+                print('游戏结束,再见!')
+                return True
+            elif cmd[0] == 'restart':
+                self.deck = Deck(self.stage_id)
+            elif cmd[0] in ('up', 'down', 'left', 'right'):
+                pos_prev = self.deck.get_cursor()
+                dot_prev = self.deck.get_dot_at_cursor()
+                color = dot_prev.get_color()
+                link_of_color = self.deck.get_link(color)
+                if self.move_selector(cmd[0]):
+                    # 移动成功, 但能不能连接要再判断
+                    if len(cmd) > 1 and cmd[1] == 'link':
+                        if dot_prev.is_end_point:
+                            # 清除旧的, 重新连线
+                            self.deck.clear_link(color)
+                        pos_now = self.deck.get_cursor()
+                        dot_now = self.deck.get_dot_at_cursor()
+                        can_connect = True
+                        if dot_now.is_end_point and dot_now.get_color() != color:
+                            # 颜色不同, 不能移动, 回退
+                            self.deck.move_cursor(*pos_prev)
+                            can_connect = False
+                        elif not dot_now.is_end_point:
+                            # 颜色不同, 但能覆盖
+                            color_now = dot_now.get_color()
+                            if color_now in self.deck.links:
+                                color_segments = self.deck.links[color_now]
+                                if pos_now in color_segments:
+                                    while color_segments[-1] != pos_now:
+                                        self.deck.clear_link_segment(color_segments[-1], color_segments[-2])
+                                        color_segments.pop()
+                                    # 当前节点也要清除
+                                    self.deck.clear_link_segment(color_segments[-1], color_segments[-2], reset_color_a=False, reset_color_b=False)
+                                    color_segments.pop()
+                        if can_connect:
+                            dot_now.set_color(color)
+                            if link_of_color == []:
+                                link_of_color.append(pos_prev)
+                            if pos_prev in link_of_color:
+                                while link_of_color[-1] != pos_prev:
+                                    self.deck.clear_link_segment(link_of_color[-1], link_of_color[-2], reset_color_b=False)
+                                    link_of_color.pop()
+                            # self.deck.get_dot(*link_of_color[-1]).set_color(color) # 补偿颜色
+                            link_of_color.append(pos_now)
+                            self.deck.set_link(color, link_of_color)
+                self.print_deck()
+            if self.check_win():
+                print(f'你赢了! 第{self.stage_id}关结束')
+                break
+
+if __name__ == '__main__':
+    stage_files = os.listdir('stages')
+    for file_name in stage_files:
+        if '-' not in file_name:
+            continue
+        game = LineGame(file_name.split('.')[0])
+        is_quit = game.play()
+        if is_quit:
+            break
+        next_stage = input('下一局? (y/n)').strip().lower()
+        if not (next_stage == '' or next_stage[0] == 'y'):
+            break
---
+++ lines/stages/1-1.py
@@ -0,0 +1,9 @@
+from .color import Color
+size = (5, 5)
+deck = [
+    [Color.RED, None, Color.GREEN, None, Color.YELLOW],
+    [None, None, Color.BLUE, None, Color.MAGENTA],
+    [None, None, None, None, None],
+    [None, Color.GREEN, None, Color.YELLOW, None],
+    [None, Color.RED, Color.BLUE, Color.MAGENTA, None],
+]
