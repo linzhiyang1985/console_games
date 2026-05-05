b5877e92ae7cc088c42a20efd0c9b9e22ae7a0c0
linzhiyang1985 <linzhiyang1985@gmail.com>
2026-05-05 12:55:07 +0800

    Delete lines/stages directory

--- lines/stages/1-1.py
+++
@@ -1,9 +0,0 @@
-from .color import Color
-size = (5, 5)
-deck = [
-    [Color.RED, None, Color.GREEN, None, Color.YELLOW],
-    [None, None, Color.BLUE, None, Color.MAGENTA],
-    [None, None, None, None, None],
-    [None, Color.GREEN, None, Color.YELLOW, None],
-    [None, Color.RED, Color.BLUE, Color.MAGENTA, None],
-]
--- lines/stages/1-2.py
+++
@@ -1,9 +0,0 @@
-from .color import Color
-size = (5, 5)
-deck = [
-    [Color.YELLOW, None, None, None, None],
-    [None, None, None, None, None],
-    [None, None, Color.GREEN, None, None],
-    [Color.BLUE, Color.GREEN, Color.RED, None, Color.YELLOW],
-    [Color.RED, None, None, None, Color.BLUE],
-]
