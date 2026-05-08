3a6954e3ec86b0a72f88656dc140b37966486305
linzhiyang1985 <linzhiyang1985@gmail.com>
2026-05-08 10:40:08 +0800

    Update bejeweled.py
    optimize screen print, add background music

--- gem_match/bejeweled.py
+++ gem_match/bejeweled.py
@@ -6,12 +6,18 @@ import time
 from copy import deepcopy
 from playsound3 import playsound
 import subprocess
+import re
+import threading
+
+
+CHINESE_CHAR_PATTERN = re.compile(r'[\u4e00-\u9fa5]{1}')  # 中文字符的Unicode范围
+WIDECHAR_PATTERN = re.compile(r'[^\u0001-\u4e00]{1}')  # 任意宽字符, 如Emoji
 
 GEM_TYPES = ['💎', '🧊', '💜', '🌸', '🌕', '🍀', '🚀'] ## 🏀🔴 🔶 '🟩',❄ '💠'  🔷 '💛'   #'🟥', '🟢', '🔵', '🟡', '🟣'
 EXPLOSED_GEM = '💥'
 SPECIAL_GEMS = {
-    'lightning_h': '⚡', #⚡
-    'lightning_v': '⬡', #❄
+    'lightning_h': '⚡',
+    'lightning_v': '❄',
     'bomb': '💣',
     'rainbow': '🌈'
 }
@@ -28,6 +34,7 @@ class BejeweledGame:
         self.game_over = False
         self.cursor_pos = (0, 0)
         self.need_help = True
+        self.prev_frame_rows = []
         self.init_board()
     
     def init_board(self):
@@ -44,34 +51,88 @@ class BejeweledGame:
     def clear(self):
         subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
 
-    def print_title(self):
-        print("=== 宝石迷阵 ===")
+    def output_title(self):
+        return ["=== 宝石迷阵 ==="]
 
-    def print_rules(self):
+    def output_rules(self):
+        rules = []
         if self.need_help:
-            print("游戏规则: 交换相邻宝石，使至少3颗相同颜色的宝石连成一线")
-            print("方向键移动光标, Shift+方向键交换宝石, 't'提示, 'q'退出游戏")
+            rules.append("游戏规则:")
+            rules.append("交换相邻宝石, 使至少3颗相同颜色的宝石连成一线")
+            rules.append("方向键移动光标, Shift+方向键交换宝石")
+            rules.append("'t'提示, 'q'退出游戏")
             self.need_help = False
+        return rules
     
-    def print_score(self):
-        print(f"得分: {self.score}\n")
+    def output_score(self):
+        return [f"得分: {self.score}"]
 
-    def print_board(self, matched_cells=[]):
-        self.clear()
-        self.print_title()
-        self.print_rules()
-        
-        self.print_score()
+    def output_board(self):
+        board_rows = []
 
         for i, row in enumerate(self.board):
+            row_str = ''
             for j, cell in enumerate(row):
-                cell_display = f"{cell}" if cell != ' ' else EXPLOSED_GEM
+                cell_display = f"{cell}" if cell != ' ' else EXPLOSED_GEM # 爆炸后的宝石显示为💥
                 if (i, j) == self.cursor_pos:
                     cell_display = f"[{cell_display}]"
                 else:
                     cell_display = f" {cell_display} "
-                print(cell_display, end=' ')
-            print()
+                row_str += cell_display
+            board_rows.append(row_str)
+        return board_rows
+
+    def move_console_cursor(self, row: int, column: int):
+        # 移动光标到指定位置, 左上角坐标为(1,1)
+        if os.name == 'nt':
+            msvcrt.putch(b'\x1b')
+            msvcrt.putch(b'[')
+            for r in f"{row}":
+                msvcrt.putch(r.encode())
+            msvcrt.putch(b';')
+            for c in f"{column}":
+                msvcrt.putch(c.encode())
+            msvcrt.putch(b'H')
+        else:
+            curses.move(row, column)
+
+    def calculate_delta_frame_row(self, new_frame_rows):
+        delta_frame_rows = []
+        for i, new_row in enumerate(new_frame_rows):
+            new_row_len = len(new_row) + len(WIDECHAR_PATTERN.findall(new_row)) # 每个中文字符/Emoji占两位宽度
+
+            old_row = self.prev_frame_rows[i] if i < len(self.prev_frame_rows) else ''
+            old_row_len = len(old_row) + len(WIDECHAR_PATTERN.findall(old_row)) # 每个中文字符/Emoji占两位宽度
+
+            if new_row != old_row:
+                new_row = new_row.ljust(max(old_row_len, new_row_len))
+                delta_frame_rows.append((i+1, new_row))
+        extra_rows_to_clean = len(self.prev_frame_rows) - len(new_frame_rows)
+        if extra_rows_to_clean > 0:
+            for i in range(len(new_frame_rows), len(self.prev_frame_rows), 1):
+                old_row_len = len(old_row) + len(WIDECHAR_PATTERN.findall(self.prev_frame_rows[i])) # 每个中文字符占两位宽度
+                delta_frame_rows.append((i+1, ' ' * old_row_len)) # 打印空格以覆盖旧行
+        return delta_frame_rows
+
+    def print_frame(self, extra_rows=[], insert_at=-1):
+        title_row = self.output_title()
+        rule_rows = self.output_rules()
+        score_row = self.output_score()
+        board_rows = self.output_board()
+
+        frame_rows = title_row + rule_rows + score_row + board_rows
+        for row in extra_rows:
+            if insert_at == -1:
+                frame_rows.append(row)
+            else:
+                frame_rows.insert(insert_at, row)
+        
+        delta_frame_rows = self.calculate_delta_frame_row(frame_rows)
+        self.prev_frame_rows = frame_rows
+
+        for i, row in delta_frame_rows:
+            self.move_console_cursor(i+1, 1)
+            print(row)
     
     def has_matches(self, board):
         for i in range(BOARD_SIZE):
@@ -81,7 +142,7 @@ class BejeweledGame:
                 if board[j][i] == board[j+1][i] == board[j+2][i]: # 检查垂直匹配
                     return True
         return False
-    
+
     def swap_gems(self, row1, col1, row2, col2):
         self.board[row1][col1], self.board[row2][col2] = self.board[row2][col2], self.board[row1][col1]
     
@@ -163,7 +224,7 @@ class BejeweledGame:
                     special_gem_row, special_gem_col = (match[0] if is_horizontal else match[-1])
                     self.board[special_gem_row][special_gem_col] = self.create_special_gem(match_count, is_horizontal)
             if not is_mute:
-                self.print_board(match)
+                self.print_frame()
                 self.play_clear()
                 time.sleep(0.5)
         for match in matches:
@@ -210,7 +271,7 @@ class BejeweledGame:
                 if exploded:
                     if not is_mute:
                         self.play_explode()
-                        self.print_board(exploded_cells)
+                        self.print_frame()
                         time.sleep(0.5)
         return True
     
@@ -229,7 +290,7 @@ class BejeweledGame:
                     empty_row -= 1
             if not is_mute and play_fill_count > 0 and not falling_played:
                 self.play_fill()
-                self.print_board()
+                self.print_frame()
                 time.sleep(0.2)
                 falling_played = True
 
@@ -242,7 +303,7 @@ class BejeweledGame:
 
             if not is_mute and play_fill_count > 0 and not new_gen_played:
                 self.play_fill()
-                self.print_board()
+                self.print_frame()
                 time.sleep(0.1)
                 new_gen_played = True
     
@@ -253,7 +314,7 @@ class BejeweledGame:
             else:
                 return SPECIAL_GEMS['lightning_v']
         elif match_length >= 5:
-            return SPECIAL_GEMS['rainbow']
+            return random.choice([SPECIAL_GEMS['bomb'], SPECIAL_GEMS['rainbow']])
         return ' '
     
     def process_matches(self, is_mute=False):
@@ -358,22 +419,53 @@ class BejeweledGame:
         return True, None
 
     def play(self):
-        self.print_title()
-        self.print_rules()
-        
+        messages = []
         while not self.game_over:
-            self.print_board()
+            self.print_frame()
             if not self.handle_input():
                 break
             self.process_matches()
             if self.check_no_more_moves()[0]:
                 self.game_over = True
-                print("没办法再移动了！")
+                messages.extend(["没办法再移动了！"])
         
-        print(f"\n游戏结束! 最终得分: {self.score}")
+        messages.extend(["", f"游戏结束! 最终得分: {self.score}"])
+        self.print_frame(messages)
+
+class LoopPlayer(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self.sound_file_path = 'sound/background.mp3'
+        self.track_length = 157
+        self.sound_handle = None
+        self.is_stop = False
+        
+    def run(self):
+        start_time = time.time()
+        self.sound_handle = playsound(self.sound_file_path, block=False)
+        while not self.is_stop:
+            if time.time() - start_time >= self.track_length: # whole sound duration
+                # play random sound
+                self.sound_handle.stop()
+                self.sound_handle = playsound(self.sound_file_path, block=False)
+            time.sleep(1)
+        self.sound_handle.stop()
+    
+    def stop(self):
+        self.is_stop = True
 
 if __name__ == "__main__":
-    game = BejeweledGame()
-    game.process_matches(is_mute=True)
-    game.score = 0
-    game.play()
+    try:
+        background_player = LoopPlayer()
+        background_player.start()
+
+        game = BejeweledGame()
+        game.process_matches(is_mute=True)
+        game.score = 0
+        game.clear()
+        game.play()
+
+    except Exception as e:
+        pass
+    finally:
+        background_player.stop()
